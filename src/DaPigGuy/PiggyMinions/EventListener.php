5ade7004c19bd09b673617350a56abd12f1902be
DaPigGuy <mcpepig123@gmail.com>
2020-03-16 23:46:23 -0700

    Fixed #3

--- src/DaPigGuy/PiggyMinions/EventListener.php
+++ src/DaPigGuy/PiggyMinions/EventListener.php
@@ -7,10 +7,10 @@ namespace DaPigGuy\PiggyMinions;
 use DaPigGuy\PiggyMinions\inventory\MinionInventory;
 use DaPigGuy\PiggyMinions\minions\MinionInformation;
 use DaPigGuy\PiggyMinions\minions\MinionType;
-use pocketmine\block\BlockIds;
 use pocketmine\entity\Entity;
-use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\item\ItemIds;
 use pocketmine\nbt\tag\ByteArrayTag;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\nbt\tag\IntTag;
@@ -27,34 +27,35 @@ class EventListener implements Listener
     }
 
     /**
-     * @priority HIGHEST
      * @ignoreCancelled true
      */
-    public function onPlace(BlockPlaceEvent $event): void
+    public function onInteract(PlayerInteractEvent $event): void
     {
         $player = $event->getPlayer();
-        $block = $event->getBlock();
-        if ($block->getId() === BlockIds::MOB_HEAD_BLOCK) {
+        if ($event->getAction() === PlayerInteractEvent::RIGHT_CLICK_BLOCK) {
             $item = $event->getItem();
-            if (($minionInformation = $item->getNamedTag()->getCompoundTag("MinionInformation")) !== null) {
-                if (($minionType = $minionInformation->getCompoundTag("MinionType")) !== null) {
-                    if (($actionType = $minionType->getTag("ActionType")) instanceof IntTag && ($targetId = $minionType->getTag("TargetID")) instanceof IntTag) {
-                        $skin = $player->getSkin();
+            if ($item->getId() === ItemIds::MOB_HEAD) {
+                if (($minionInformation = $item->getNamedTag()->getCompoundTag("MinionInformation")) !== null) {
+                    if (($minionType = $minionInformation->getCompoundTag("MinionType")) !== null) {
+                        if (($actionType = $minionType->getTag("ActionType")) instanceof IntTag && ($targetId = $minionType->getTag("TargetID")) instanceof IntTag) {
+                            $skin = $player->getSkin();
 
-                        $nbt = Entity::createBaseNBT($block->add(0.5, 0, 0.5));
-                        $nbt->setTag(new CompoundTag("Skin", [
-                            new StringTag("Name", $skin->getSkinId()),
-                            new ByteArrayTag("Data", $skin->getSkinData()),
-                            new ByteArrayTag("CapeData", $skin->getCapeData()),
-                            new StringTag("GeometryName", $skin->getGeometryName()),
-                            new ByteArrayTag("GeometryData", $skin->getGeometryData())
-                        ]));
-                        $nbt->setTag((new MinionInformation($player->getUniqueId(), new MinionInventory(), new MinionType($actionType->getValue(), $targetId->getValue()), $minionInformation->getInt("MinionLevel", 1), $minionInformation->getInt("ResourcesCollected", 0), time()))->toNBT());
+                            $nbt = Entity::createBaseNBT($event->getBlock()->getSide($event->getFace())->add(0.5, 0, 0.5));
+                            $nbt->setTag(new CompoundTag("Skin", [
+                                new StringTag("Name", $skin->getSkinId()),
+                                new ByteArrayTag("Data", $skin->getSkinData()),
+                                new ByteArrayTag("CapeData", $skin->getCapeData()),
+                                new StringTag("GeometryName", $skin->getGeometryName()),
+                                new ByteArrayTag("GeometryData", $skin->getGeometryData())
+                            ]));
+                            $nbt->setTag((new MinionInformation($player->getUniqueId(), new MinionInventory(), new MinionType($actionType->getValue(), $targetId->getValue()), $minionInformation->getInt("MinionLevel", 1), $minionInformation->getInt("ResourcesCollected", 0), time()))->toNBT());
 
-                        $entity = Entity::createEntity("MinionEntity", $player->getLevel(), $nbt);
-                        $entity->spawnToAll();
+                            $entity = Entity::createEntity("MinionEntity", $player->getLevel(), $nbt);
+                            $entity->spawnToAll();
 
-                        $event->setCancelled();
+                            $player->getInventory()->setItemInHand($item->setCount($item->getCount() - 1));
+                            $event->setCancelled();
+                        }
                     }
                 }
             }
